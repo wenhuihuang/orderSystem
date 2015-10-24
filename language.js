5b32c31061f431e6c80299e9ad42a5f8401a8592
mrc <mairongchao@163.com>
2015-10-24 08:39:26 +0800

    a

--- language.js
+++ language.js
@@ -4,27 +4,31 @@ define(function(require) {
 	var Baas = require("$UI/demo/baas/baas");
 	
 	var lan = {	
-		//返回参数"languages":[{"languageId":"00010000000000000101","languageName":"中文"}]}
+		//返回参数"{languages":[{"languageId":"00010000000000000101","languageName":"中文"}]}
 		//输入参数	{'ip':xxx}
 		getLanguages:function(data){
 			var success = function(param){
+				//在这里要保存ip到本地和当前语言到本地
+				localStorage.setItem('ip', data.ip);
+				localStorage.setItem('languageId',param.languages[0].languageId);
 				return param;
 			}
+			
 			Baas.sendRequest({
 				"url" : data.ip + 'LanguageServlet.do?func=getLanguages',
 				"dataType": "json",
 				"success" : success
 			});		
 		},
-		//返加参数{"code":"1","result":"在用"}
-		//输入参数{'ip':xxx,'languageId',xxx,'internalCode':xxx}
+		//返加参数 字符串
+		//输入参数{'internalCode':xxx}
 		getTranslation:function(data){
 			var success = function(param){
 				return param.result;
 			}
 			Baas.sendRequest({
 				//getTranslation&languageId=00010000000000000101&internalCode=USING
-				"url" : data.ip + 'getTranslation&languageId='+data.languageId+'&internalCode='+data.internalCode,
+				"url" : localStorage.getItem('ip') + 'getTranslation&languageId='+localStorage.getItem('languageId')+'&internalCode='+data.internalCode,
 				"dataType": "json",
 				"success" : success
 			});	
