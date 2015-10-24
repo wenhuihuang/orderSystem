7673e6ac53bb16b35e4b898bec5940cef1e52a88
mrc <mairongchao@163.com>
2015-10-24 16:27:32 +0800

    a

--- myBaas.js
+++ myBaas.js
@@ -5,26 +5,30 @@ define(function(require) {
 	
 	var myBaas = {	
 		cleanRoom:function(data){
+			var obj;
 			var success = function(param){
-				return param;
+				obj =  param;
 			}
 			
 			Baas.sendRequest({
 				"url" : data.ip + 'RoomFunctionServlet.do?func=cleanRoom&currentBillMasterId='+data.billMasterId+'&checkEmpCodeId='+data.userId+'&currentConsumeRoomId='+data.consumeRoomId,
 				"dataType": "json",
 				"success" : success
-			});		
+			});
+			return obj;
 		},
 		uncheckBill:function(data){
+			var obj;
 			var success = function(param){
-				return param;
+				obj = param;
 			}
 			
 			Baas.sendRequest({
 				"url" : data.ip + 'RoomFunctionServlet.do?func=uncheckBill&currentBillMasterId='+data.billMasterId+'&checkEmpCodeId='+data.userId,
 				"dataType": "json",
 				"success" : success
-			});		
+			});	
+			return obj;
 		}
 		//RoomFunctionServlet.do?func=uncheckBill&currentBillMasterId=xxx&checkEmpCodeId=xxx
 	};
