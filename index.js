147327a2f0a90913b4e5ab125903009289d2781c
hwh <hwh@163.com>
2015-11-13 10:47:00 +0800

    ff

--- index.js
+++ index.js
@@ -1424,10 +1424,10 @@ define(function(require){
     	var newNum = this.getElementByXid('input1');//新开台输入人数框
     	event.preventDefault();
         clearTimeout(timeOutEvent);//清除定时器   
-        if(timeOutEvent!=0 && lang==false){              
+        if(timeOutEvent!=0){              
         //这里写要执行的内容（尤如onclick事件）  
        
-		if(action == undefined ||action == ''){
+		if(action == undefined ||action == ''  && lang==false){
 		 clearTimeout(timeOutEvent);//清除定时器  
         timeOutEvent = 0; 
         var currentDeskData = this.comp('currentDeskData');
